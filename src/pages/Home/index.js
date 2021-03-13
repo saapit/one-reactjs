8fd1617c8280034fe535bab9159931b98145fde1
sapit <syafiqshahrim19@gmail.com>
2021-03-13 14:46:07 +0800

    calculator

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,28 +1,84 @@
 import React, { useState } from 'react'
-import './Home.scss'
+import './Home.css'
 import {Button, Input, Screen} from '../../components';
 
 const Home = () => {
-    const [number1, setNumber1] = useState(0)
-    const [number2, setNumber2] = useState(0)
+    const [input, setInput] = useState("");
+    const calcBtns = [];
+    [9, 8, 7, 6, 5, 4, 3, 2, 1, 0, ".", "%"].forEach((item) => {
+        calcBtns.push(
+            <button onClick ={(e) => {
+                setInput(input + e.target.value);
+            }}
+            value={item}
+            key={item}>
+                {" "}
+                {item}
+            </button>
+        );
+    });
 
     
-    const add = () => {
-        let total = 0;
-        console.log(number1)
-        console.log(number2)
-        total = number1 + number2;
-        console.log(total)
-    }
+  
 
     return (
-        <div className="con-1">
-            <p>Hello World</p>
-            <Input label="Enter number 1" onChange={(e) => setNumber1(Number(e.target.value))}/>
-            <Input label="Enter number 2" onChange={(e) => setNumber2(Number(e.target.value))}/>
-            {/* <Button title="Add" onClick={add}/> */}
-            <Screen value={number1+number2}/>
+        <div className="wrapper">
+        {" "}
+        <div className="show-input">{input}</div>
+        <div className="digits flex">{calcBtns}</div>
+        <div className="modifiers subgrid">
+          {/* clear button */}
+  
+          <button onClick={() => setInput(input.substr(0, input.length - 1))}>
+            Clear
+          </button>
+  
+          {/* clear all */}
+          <button onClick={() => setInput("")} value="">
+            AC
+          </button>
         </div>
+        <div className="operations subgrid">
+          {/* add button */}
+          <button onClick={(e) => setInput(input + e.target.value)} value="+">
+            +
+          </button>
+  
+          {/* minus btn */}
+          <button onClick={(e) => setInput(input + e.target.value)} value="-">
+            {" "}
+            -{" "}
+          </button>
+  
+          <button onClick={(e) => setInput(input + e.target.value)} value="*">
+            {" "}
+            *
+          </button>
+  
+          <button onClick={(e) => setInput(input + e.target.value)} value="/">
+            {" "}
+            /
+          </button>
+          {/* "=" btn */}
+          <button
+            onClick={(e) => {
+              try {
+                setInput(
+                  String(eval(input)).length > 3 &&
+                    String(eval(input)).includes(".")
+                    ? String(eval(input).toFixed(4))
+                    : String(eval(input))
+                );
+              } catch (e) {
+                console.log(e);
+              }
+            }}
+            value="="
+          >
+            =
+          </button>
+        </div>
+      </div>
     )
 }
 
